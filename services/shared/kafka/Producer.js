459b7a912c712ac8b91806dafa6b2b84be6befbf
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-12-17 00:54:16 +0000

    feat: refactor shared kafka utility functions

---
+++ services/shared/kafka/Producer.js
@@ -0,0 +1,35 @@
+function Producer({ Kafka, metadataBrokerList }) {
+  return new Promise((resolve, reject) => {
+    const producer = new Kafka.Producer({
+      //'debug' : 'all',
+      'metadata.broker.list': metadataBrokerList,
+      'dr_cb': true  //delivery report callback
+    })
+    
+    producer.on('event.log', function(log) {
+      console.log(log)
+    })
+    
+    producer.on('event.error', function(err) {
+      console.error('Error from producer')
+      console.error(err)
+    })
+    
+    producer.on('delivery-report', function(err, report) {
+      console.log('delivery-report: ' + JSON.stringify(report))
+    })
+    
+    producer.on('ready', function(arg) {
+      console.log('producer ready.' + JSON.stringify(arg))
+      resolve(producer)
+    })
+    
+    producer.on('disconnected', function(arg) {
+      console.log('producer disconnected. ' + JSON.stringify(arg))
+    })
+    
+    producer.connect()
+  })
+}
+
+module.exports = Producer
