3f761e512f6454d5a1617d9a96d515adbd86afe5
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-12-17 00:55:01 +0000

    feat: integrate with order service

--- services/apiGateway/index.js
+++ services/apiGateway/index.js
@@ -1,7 +1,48 @@
-const service = require('./service')
 
-if (module.id === require.main.id) {
-  service.start()
+const app = require('express')()
+const config = require('./config')
+const morgan = require('morgan')('tiny')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+let runningService
+
+app.use(morgan)
+app.use(bodyParser.json())
+app.use('/order', routes.order)
+
+app.get('/health', (req, res) => {
+  return res.status(200).json({
+    app: 'API Gateway',
+    status: 'UP'
+  })
+})
+
+async function start() {
+  return new Promise((resolve, reject) => {
+    runningService = app.listen(config.get('port'), config.get('hostname'), () => {
+      console.log(`API Gateway service running at http://${config.get('hostname')}:${config.get('port')}/\n`)
+      resolve()
+    })
+  })
 }
 
-module.exports = service
+async function close() {
+  return new Promise((resolve, reject) => {
+    if (runningService) {
+      runningService.close(() => {
+      })
+      resolve()
+    }
+    reject()
+  })
+}
+
+if (module.id === require.main.id) {
+  start()
+}
+
+module.exports = {
+  start,
+  close
+}
