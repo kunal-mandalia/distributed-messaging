75f2816412b1540b0f53b84d6ca91a3075ccfcaa
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-01-23 01:10:11 +0000

    feat: handle create order command

--- services/order/kafka/consumersDefinition.js
+++ services/order/kafka/consumersDefinition.js
@@ -1,10 +1,51 @@
+const { encodeMessage, decodeMessage } = require('../../shared/kafka/message')
+const { RESOURCE_MAP, OPERATION_MAP, MESSAGE_TYPE_MAP } = require('../../shared/constants')
+
+const { ORDER } = RESOURCE_MAP
+const { CREATE, CREATED } = OPERATION_MAP
+const { COMMAND, EVENT } = MESSAGE_TYPE_MAP
+
 const consumersDefinition = [
   {
     consumerGroupId: 'order-service-consumer',
     topicNames: ['order'],
     handler: producer => consumer => async message => {
       console.log('order-service-consumer received message')
+      console.log(message)
       console.log(message.value.toString())
+
+      const { aggregateId, resource, operation, type, payload } = decodeMessage(message)
+
+      if (resource !== ORDER) {
+        console.log(`ignoring messaging ${aggregateId}`)
+        return
+      }
+
+      if (operation === CREATE && type === COMMAND) {
+
+          // update db idempotently
+          
+          // produce kafka message
+          const eventMessage = encodeMessage({
+            aggregateId,
+            resource: ORDER,
+            operation: CREATED,
+            type: EVENT,
+            payload
+          })
+          producer.produce('order', -1, eventMessage, aggregateId)
+          
+          // ack message
+          await consumer.commitMessage(message)
+        }
+
+        console.log(`unhandled message:
+          ${aggregateId}
+          ${resource}
+          ${operation}
+          ${type}
+          ${JSON.stringify(payload, null, 4)}
+        `)
     }
   }
 ]
