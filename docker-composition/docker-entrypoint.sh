173a758e2ab9a17db15d151d2bdd8a985058c621
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-03-15 20:54:22 +0000

    feat: wait until all services are ready

--- docker-composition/docker-entrypoint.sh
+++ docker-composition/docker-entrypoint.sh
@@ -1,30 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+delay=1
+maxAttempts=150
 
 # (serviceName, host, port)
-wait_for_service() {
+wait_until_online() {
     echo Waiting for $1
-    for i in `seq 1 60`;
+    for i in `seq 1 $maxAttempts`;
     do
-        nc -z $2 $3 && echo Success && return
-        echo -n .
-        sleep 1
+        nc -z $2 $3 && echo $1 is online && return
+        echo -ne "."
+        sleep $delay
     done
     echo Failed waiting for $1 && exit 1
 }
 
+# (serviceName, domain, port)
+wait_for_service() {
+  echo Waiting for $1 $2:$3
+  for i in `seq 1 $maxAttempts`;
+  do
+    RESPONSE=$(curl -H "Content-Type:application/json" -H "Accept:application/json" --silent http://$2:$3/readiness)
+    if [ "$?" == "0" ]; then 
+    echo $1 health: $RESPONSE
+      if [[ $RESPONSE == *"\"ready\":true"* ]]
+      then
+        echo $1 is ready
+        return
+      else
+        echo -ne "."
+        sleep $delay
+      fi
+    else
+      echo -ne "."
+      sleep $delay
+    fi
+  done
+  echo Failed waiting for $1 && exit 1
+}
+
+
 if [ "$1" = "start" ]
 then
-  echo initializing
-  wait_for_service kafkaZookeeper localhost 2181
-  wait_for_service kafkaBroker localhost 9092
-  wait_for_service mongodb localhost 27017
+  echo Started waiting for all services
+
+  wait_until_online kafka localhost 9092
+  wait_until_online mongo localhost 27017
+  
   wait_for_service apiGateway localhost 8900
   wait_for_service inventory localhost 8901
   wait_for_service order localhost 8902
   wait_for_service notification localhost 8903
-  echo starting
+  
+  echo Services are up and running!
 fi
-
-exec "$@"
