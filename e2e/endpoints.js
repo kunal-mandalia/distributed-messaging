d61a349e005f81db7dcae1339800156982c876a9
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-03-17 14:44:16 +0000

    fix: endopoints

--- e2e/endpoints.js
+++ e2e/endpoints.js
@@ -2,9 +2,9 @@ const env = process.env.CONFIG_ENV === 'CI' ? 'CI' : 'local'
 
 const endpoints = {
   CI: {
-    apiGateway: 'http://api-gateway:8090',
-    notification: 'ws://notification:8094',
-    mongo: 'mongodb://mongo:27017/'
+    apiGateway: 'http://localhost:8090',
+    notification: 'ws://localhost:8094',
+    mongo: 'mongodb://localhost:27017/'
   },
   local: {
     apiGateway: 'http://0.0.0.0:8900',
